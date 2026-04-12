1f7149cff87dbb170feaa6bf4287c0c8d99ff0fe
Mr-Trade-vs <stevanandradebecerra@gmail.com>
2026-04-12 13:26:06 -0500

    Agregando funcionalidad que permite actualizar datos de Beneficiarios que ya forman parte dle sistema. Esto solo lo pueden desempeñar administradores y secretarias

--- Gestion_de_Casos_T4/beneficiary/forms.py
+++ Gestion_de_Casos_T4/beneficiary/forms.py
@@ -38,3 +38,38 @@ class BeneficiaryForm(forms.ModelForm):
             'phone': 'Teléfono',
             'email': 'Correo electrónico',
         }
+
+class Update_Beneficiary_Form(forms.ModelForm):
+    
+    class Meta:
+        model = Beneficiary
+        fields = ['name', 'id', 'location', 'phone', 'email']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Nombre completo',
+            }),
+            'id' : forms.TextInput(attrs={
+                'class' : 'form-control',
+                'placeholder' : 'C.C',
+            }),
+            'location': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ciudad, Departamento',
+            }),
+            'phone': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ej: 3001234567',
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'correo@ejemplo.com',
+            }),
+        }
+        labels = {
+            'name': 'Nombre',
+            'id' : 'Número de Identificación',
+            'location': 'Ubicación',
+            'phone': 'Teléfono',
+            'email': 'Correo electrónico',
+        }
