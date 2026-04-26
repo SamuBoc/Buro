0f91dd52a2fb29f973e456a9c2b018be8f9deed5
Mr-Trade-vs <stevanandradebecerra@gmail.com>
2026-04-26 01:29:19 -0500

    feat(cite): agregando la funcionalidad de reprogramar una cita, para ello se realizo un nuevo template y se adicciono un form que unicamente tiene el campo de fecha asignada. Facilitando el proceso de unicamente actualziar el atributo de fecha de asignación de una cita sin compromenter toda la entidad. Además de que el programa muestra la actual fecha asignada al usuario para que decida o no elegir una nueva fecha

--- Gestion_de_Casos_T4/cite/urls.py
+++ Gestion_de_Casos_T4/cite/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('beneficiario/<str:beneficiary_id>/agendar/', views.create_cite, name='create_cite'),
     path('beneficiario/<str:beneficiary_id>/', views.beneficiary_cites, name='beneficiary_cites'),
+    path('beneficiario/<str:pk>/reprogramar/', views.reschedule_cite, name='reschedule_cite')
 ]
