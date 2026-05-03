8f4d3f64c44539daa0556ed02dcb8be0f74e73b0
ste-ven1201 <haroldsteven20071201@gmail.com>
2026-05-03 11:35:27 -0500

    feat(cases): Se hizo la implementacion de los templates y la logica de control

---
+++ Gestion_de_Casos_T4/cite/migrations/0003_merge_0002_branches.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cite', '0002_alter_cite_state_cite'),
+        ('cite', '0002_cite_reminder_sent'),
+    ]
+
+    operations = []
