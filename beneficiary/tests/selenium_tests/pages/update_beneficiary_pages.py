c831ffe337542d000be0a8632efb646c630e7301
Mr-Trade-vs <stevanandradebecerra@gmail.com>
2026-05-25 12:28:03 -0500

    test(beneficiary): adding selenium test to HU-5, Both test pass without any trouble. First test make an successful update of one beneficiary, second test cancel modifications and system don't change original information

--- beneficiary/tests/selenium_tests/pages/update_beneficiary_pages.py
+++ beneficiary/tests/selenium_tests/pages/update_beneficiary_pages.py
@@ -4,7 +4,7 @@ from .base_page import BasePage
 
 import time
 
-class RegisterBeneficiarySelenium(BasePage) :
+class UpdateBeneficiarySelenium(BasePage) :
     BENEFICIARY_MODULE = (By.CLASS_NAME, "beneficiary-module")
 
     ACTIONS_BENEFICIARY = (By.ID, "actions-user")
@@ -17,7 +17,20 @@ class RegisterBeneficiarySelenium(BasePage) :
 
     BUTTON_SUBMIT = (By.ID, "update-beneficiary")
 
+    LOCATION_FIELD = (By.ID, "location-beneficiary")
+
+    PHONE_FIELD = (By.ID, "phone-beneficiary")
+
     GO_TO_BINNACLE = (By.ID, "binnacle-beneficiary")
+
+    ACTION_DONE = (By.ID, "description-action-UPDATED-2")
+
+    # Cancel Modifications
+    CANCEL_BUTTON = (By.ID, "cancel-update")
+
+    MODIFY_NAME = (By.ID, "id_name")
+
+    FIELD_NAME = (By.ID, "name-beneficiary")
 
     def go_to_homepage(self, url = "http://127.0.0.1:8000/"):
         self.driver.get(url)
@@ -38,5 +51,27 @@ class RegisterBeneficiarySelenium(BasePage) :
     def make_update(self):
         self.click(self.BUTTON_SUBMIT)
 
+    def get_location_modified(self):
+        location = self.find_element(self.LOCATION_FIELD)
+        return location.text
+
+    def get_phone_modified(self):
+        phone = self.find_element(self.PHONE_FIELD)
+        return phone.text
+
     def go_to_binnacle(self):
         self.click(self.GO_TO_BINNACLE)
+
+    def get_action_done(self):
+        action = self.find_element(self.ACTION_DONE)
+        return action.text
+    
+    def modify_some_fields_to_cancel(self):
+        self.enter_text(self.MODIFY_NAME, "No soy real")
+
+    def cancel_update(self):
+        self.click(self.CANCEL_BUTTON)
+
+    def get_name(self):
+        name = self.find_element(self.FIELD_NAME)
+        return name.text
